5ec5fbf71be5b3a20d5eadea47df6823f15670bc
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2021-09-13 13:22:41 +0800

    Add project files.

---
+++ JMCSignalInterupt/JMCSignalInteruptForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace JMCSignalInterupt
+{
+    partial class JMCSignalInteruptForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartAsyncBtnOne = new System.Windows.Forms.Button();
+            this.StartAsyncBtnTwo = new System.Windows.Forms.Button();
+            this.StartAsyncBtnThree = new System.Windows.Forms.Button();
+            this.CancelAsyncBtnOne = new System.Windows.Forms.Button();
+            this.CancelAsyncBtnTwo = new System.Windows.Forms.Button();
+            this.CancelAsyncBtnThree = new System.Windows.Forms.Button();
+            this.ProgressBarOne = new System.Windows.Forms.ProgressBar();
+            this.ProgressBarTwo = new System.Windows.Forms.ProgressBar();
+            this.ProgressBarThree = new System.Windows.Forms.ProgressBar();
+            this.BGWorkerOne = new System.ComponentModel.BackgroundWorker();
+            this.BGWorkerTwo = new System.ComponentModel.BackgroundWorker();
+            this.LblOne = new System.Windows.Forms.Label();
+            this.LblTwo = new System.Windows.Forms.Label();
+            this.LblThree = new System.Windows.Forms.Label();
+            this.StatusLblOne = new System.Windows.Forms.Label();
+            this.StatusLblTwo = new System.Windows.Forms.Label();
+            this.StatusLblThree = new System.Windows.Forms.Label();
+            this.BGWorkerThree = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // StartAsyncBtnOne
+            // 
+            this.StartAsyncBtnOne.Location = new System.Drawing.Point(35, 130);
+            this.StartAsyncBtnOne.Name = "StartAsyncBtnOne";
+            this.StartAsyncBtnOne.Size = new System.Drawing.Size(75, 23);
+            this.StartAsyncBtnOne.TabIndex = 0;
+            this.StartAsyncBtnOne.Text = "Start";
+            this.StartAsyncBtnOne.UseVisualStyleBackColor = true;
+            this.StartAsyncBtnOne.Click += new System.EventHandler(this.StartAsyncBtnOne_Click);
+            // 
+            // StartAsyncBtnTwo
+            // 
+            this.StartAsyncBtnTwo.Location = new System.Drawing.Point(35, 263);
+            this.StartAsyncBtnTwo.Name = "StartAsyncBtnTwo";
+            this.StartAsyncBtnTwo.Size = new System.Drawing.Size(75, 23);
+            this.StartAsyncBtnTwo.TabIndex = 1;
+            this.StartAsyncBtnTwo.Text = "Start";
+            this.StartAsyncBtnTwo.UseVisualStyleBackColor = true;
+            this.StartAsyncBtnTwo.Click += new System.EventHandler(this.StartAsyncBtnTwo_Click);
+            // 
+            // StartAsyncBtnThree
+            // 
+            this.StartAsyncBtnThree.Location = new System.Drawing.Point(35, 392);
+            this.StartAsyncBtnThree.Name = "StartAsyncBtnThree";
+            this.StartAsyncBtnThree.Size = new System.Drawing.Size(75, 23);
+            this.StartAsyncBtnThree.TabIndex = 2;
+            this.StartAsyncBtnThree.Text = "Start";
+            this.StartAsyncBtnThree.UseVisualStyleBackColor = true;
+            this.StartAsyncBtnThree.Click += new System.EventHandler(this.StartAsyncBtnThree_Click);
+            // 
+            // CancelAsyncBtnOne
+            // 
+            this.CancelAsyncBtnOne.Location = new System.Drawing.Point(155, 129);
+            this.CancelAsyncBtnOne.Name = "CancelAsyncBtnOne";
+            this.CancelAsyncBtnOne.Size = new System.Drawing.Size(75, 23);
+            this.CancelAsyncBtnOne.TabIndex = 3;
+            this.CancelAsyncBtnOne.Text = "Cancel";
+            this.CancelAsyncBtnOne.UseVisualStyleBackColor = true;
+            this.CancelAsyncBtnOne.Click += new System.EventHandler(this.CancelAsyncBtnOne_Click);
+            // 
+            // CancelAsyncBtnTwo
+            // 
+            this.CancelAsyncBtnTwo.Location = new System.Drawing.Point(155, 262);
+            this.CancelAsyncBtnTwo.Name = "CancelAsyncBtnTwo";
+            this.CancelAsyncBtnTwo.Size = new System.Drawing.Size(75, 23);
+            this.CancelAsyncBtnTwo.TabIndex = 4;
+            this.CancelAsyncBtnTwo.Text = "Cancel";
+            this.CancelAsyncBtnTwo.UseVisualStyleBackColor = true;
+            this.CancelAsyncBtnTwo.Click += new System.EventHandler(this.CancelAsyncBtnTwo_Click);
+            // 
+            // CancelAsyncBtnThree
+            // 
+            this.CancelAsyncBtnThree.Location = new System.Drawing.Point(155, 391);
+            this.CancelAsyncBtnThree.Name = "CancelAsyncBtnThree";
+            this.CancelAsyncBtnThree.Size = new System.Drawing.Size(75, 23);
+            this.CancelAsyncBtnThree.TabIndex = 5;
+            this.CancelAsyncBtnThree.Text = "Cancel";
+            this.CancelAsyncBtnThree.UseVisualStyleBackColor = true;
+            this.CancelAsyncBtnThree.Click += new System.EventHandler(this.CancelAsyncBtnThree_Click);
+            // 
+            // ProgressBarOne
+            // 
+            this.ProgressBarOne.Location = new System.Drawing.Point(35, 79);
+            this.ProgressBarOne.Name = "ProgressBarOne";
+            this.ProgressBarOne.Size = new System.Drawing.Size(195, 23);
+            this.ProgressBarOne.TabIndex = 6;
+            // 
+            // ProgressBarTwo
+            // 
+            this.ProgressBarTwo.Location = new System.Drawing.Point(35, 218);
+            this.ProgressBarTwo.Name = "ProgressBarTwo";
+            this.ProgressBarTwo.Size = new System.Drawing.Size(195, 23);
+            this.ProgressBarTwo.TabIndex = 7;
+            // 
+            // ProgressBarThree
+            // 
+            this.ProgressBarThree.Location = new System.Drawing.Point(35, 358);
+            this.ProgressBarThree.Name = "ProgressBarThree";
+            this.ProgressBarThree.Size = new System.Drawing.Size(195, 23);
+            this.ProgressBarThree.TabIndex = 8;
+            // 
+            // BGWorkerOne
+            // 
+            this.BGWorkerOne.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorkerOne_DoWork);
+            this.BGWorkerOne.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorkerOne_ProgressChanged);
+            this.BGWorkerOne.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorkerOne_RunWorkerCompleted);
+            // 
+            // BGWorkerTwo
+            // 
+            this.BGWorkerTwo.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorkerTwo_DoWork);
+            this.BGWorkerTwo.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorkerTwo_ProgressChanged);
+            this.BGWorkerTwo.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorkerTwo_RunWorkerCompleted);
+            // 
+            // LblOne
+            // 
+            this.LblOne.AutoSize = true;
+            this.LblOne.Location = new System.Drawing.Point(32, 39);
+            this.LblOne.Name = "LblOne";
+            this.LblOne.Size = new System.Drawing.Size(126, 13);
+            this.LblOne.TabIndex = 9;
+            this.LblOne.Text = "Background Worker One";
+            // 
+            // LblTwo
+            // 
+            this.LblTwo.AutoSize = true;
+            this.LblTwo.Location = new System.Drawing.Point(32, 184);
+            this.LblTwo.Name = "LblTwo";
+            this.LblTwo.Size = new System.Drawing.Size(127, 13);
+            this.LblTwo.TabIndex = 10;
+            this.LblTwo.Text = "Background Worker Two";
+            // 
+            // LblThree
+            // 
+            this.LblThree.AutoSize = true;
+            this.LblThree.Location = new System.Drawing.Point(32, 323);
+            this.LblThree.Name = "LblThree";
+            this.LblThree.Size = new System.Drawing.Size(134, 13);
+            this.LblThree.TabIndex = 11;
+            this.LblThree.Text = "Background Worker Three";
+            // 
+            // StatusLblOne
+            // 
+            this.StatusLblOne.AutoSize = true;
+            this.StatusLblOne.Location = new System.Drawing.Point(165, 38);
+            this.StatusLblOne.Name = "StatusLblOne";
+            this.StatusLblOne.Size = new System.Drawing.Size(44, 13);
+            this.StatusLblOne.TabIndex = 12;
+            this.StatusLblOne.Text = "(Ready)";
+            // 
+            // StatusLblTwo
+            // 
+            this.StatusLblTwo.AutoSize = true;
+            this.StatusLblTwo.Location = new System.Drawing.Point(166, 183);
+            this.StatusLblTwo.Name = "StatusLblTwo";
+            this.StatusLblTwo.Size = new System.Drawing.Size(44, 13);
+            this.StatusLblTwo.TabIndex = 13;
+            this.StatusLblTwo.Text = "(Ready)";
+            // 
+            // StatusLblThree
+            // 
+            this.StatusLblThree.AutoSize = true;
+            this.StatusLblThree.Location = new System.Drawing.Point(169, 322);
+            this.StatusLblThree.Name = "StatusLblThree";
+            this.StatusLblThree.Size = new System.Drawing.Size(44, 13);
+            this.StatusLblThree.TabIndex = 14;
+            this.StatusLblThree.Text = "(Ready)";
+            // 
+            // BGWorkerThree
+            // 
+            this.BGWorkerThree.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorkerThree_DoWork);
+            this.BGWorkerThree.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorkerThree_ProgressChanged);
+            this.BGWorkerThree.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorkerThree_RunWorkerCompleted);
+            // 
+            // JMCSignalInteruptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 450);
+            this.Controls.Add(this.StatusLblThree);
+            this.Controls.Add(this.StatusLblTwo);
+            this.Controls.Add(this.StatusLblOne);
+            this.Controls.Add(this.LblThree);
+            this.Controls.Add(this.LblTwo);
+            this.Controls.Add(this.LblOne);
+            this.Controls.Add(this.ProgressBarThree);
+            this.Controls.Add(this.ProgressBarTwo);
+            this.Controls.Add(this.ProgressBarOne);
+            this.Controls.Add(this.CancelAsyncBtnThree);
+            this.Controls.Add(this.CancelAsyncBtnTwo);
+            this.Controls.Add(this.CancelAsyncBtnOne);
+            this.Controls.Add(this.StartAsyncBtnThree);
+            this.Controls.Add(this.StartAsyncBtnTwo);
+            this.Controls.Add(this.StartAsyncBtnOne);
+            this.KeyPreview = true;
+            this.Name = "JMCSignalInteruptForm";
+            this.Text = "JMC Signal Interupt";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.JMCSignalInteruptForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartAsyncBtnOne;
+        private System.Windows.Forms.Button StartAsyncBtnTwo;
+        private System.Windows.Forms.Button StartAsyncBtnThree;
+        private System.Windows.Forms.Button CancelAsyncBtnOne;
+        private System.Windows.Forms.Button CancelAsyncBtnTwo;
+        private System.Windows.Forms.Button CancelAsyncBtnThree;
+        private System.Windows.Forms.ProgressBar ProgressBarOne;
+        private System.Windows.Forms.ProgressBar ProgressBarTwo;
+        private System.Windows.Forms.ProgressBar ProgressBarThree;
+        private System.ComponentModel.BackgroundWorker BGWorkerOne;
+        private System.ComponentModel.BackgroundWorker BGWorkerTwo;
+        private System.Windows.Forms.Label LblOne;
+        private System.Windows.Forms.Label LblTwo;
+        private System.Windows.Forms.Label LblThree;
+        private System.Windows.Forms.Label StatusLblOne;
+        private System.Windows.Forms.Label StatusLblTwo;
+        private System.Windows.Forms.Label StatusLblThree;
+        private System.ComponentModel.BackgroundWorker BGWorkerThree;
+    }
+}
+
---
+++ JMCSignalInterupt/JMCSignalInteruptForm.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JMCSignalInterupt
+{
+    public partial class JMCSignalInteruptForm : Form
+    {
+        public JMCSignalInteruptForm()
+        {
+            InitializeComponent();
+            // Worker one.
+            BGWorkerOne.WorkerReportsProgress = true;
+            BGWorkerOne.WorkerSupportsCancellation = true;
+            
+            BGWorkerTwo.WorkerReportsProgress = true;
+            BGWorkerTwo.WorkerSupportsCancellation = true;
+
+            BGWorkerThree.WorkerReportsProgress = true;
+            BGWorkerThree.WorkerSupportsCancellation = true;
+        }
+
+        #region Buttons
+
+        #region B1
+
+        /// <summary>
+        /// Tell the worker to start working or pick up where he left off previously.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartAsyncBtnOne_Click(object sender, EventArgs e)
+        {
+            int value = 0;
+
+
+            if (ProgressBarOne.Value != 100)
+            {
+                value = ProgressBarOne.Value;
+            }
+
+            if (BGWorkerOne.IsBusy != true)
+            {
+                // Start the asyncronous operation.
+                BGWorkerOne.RunWorkerAsync(value);
+
+                StatusLblOne.Text = "(Working)";
+                StatusLblOne.ForeColor = Color.Blue;
+            }
+        }
+
+        /// <summary>
+        /// Tell the worker to stop.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelAsyncBtnOne_Click(object sender, EventArgs e)
+        {
+            if (BGWorkerOne.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asyncronous operation.
+                BGWorkerOne.CancelAsync();
+            }
+        }
+
+        #endregion
+
+        #region B2
+
+        /// <summary>
+        /// Tell the worker to start working or pick up where he left off previously.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartAsyncBtnTwo_Click(object sender, EventArgs e)
+        {
+            int value = 0;
+            
+            if (ProgressBarTwo.Value != 100)
+            {
+                value = ProgressBarTwo.Value;
+            }
+
+            if (BGWorkerTwo.IsBusy != true)
+            {
+                // Start the asyncronous operation.
+                BGWorkerTwo.RunWorkerAsync(value);
+
+                StatusLblTwo.Text = "(Working)";
+                StatusLblTwo.ForeColor = Color.Blue;
+            }
+            
+        }
+
+        /// <summary>
+        /// Tell the worker to stop.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelAsyncBtnTwo_Click(object sender, EventArgs e)
+        {
+            if (BGWorkerTwo.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asyncronous operation.
+                BGWorkerTwo.CancelAsync();
+            }
+        }
+        #endregion
+
+        #region B3
+
+        /// <summary>
+        /// Tell the worker to start working or pick up where he left off previously.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartAsyncBtnThree_Click(object sender, EventArgs e)
+        {
+            int value = 0;
+            
+            if (ProgressBarThree.Value != 100)
+            {
+                value = ProgressBarThree.Value;
+            }
+
+            if (BGWorkerThree.IsBusy != true)
+            {
+                // Start the asyncronous operation.
+                BGWorkerThree.RunWorkerAsync(value);
+
+                StatusLblThree.Text = "(Working)";
+                StatusLblThree.ForeColor = Color.Blue;
+            }
+            
+        }
+
+        /// <summary>
+        /// Tell the worker to stop.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelAsyncBtnThree_Click(object sender, EventArgs e)
+        {
+            if (BGWorkerThree.WorkerSupportsCancellation == true)
+            {
+                BGWorkerThree.CancelAsync();
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region WorkerOne
+
+        /// <summary>
+        /// Tell the worker what to do.
+        /// <br></br>
+        /// Calls the ReportProgress method.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerOne_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            int value = (int)e.Argument;
+
+            for (int i = value; i <= 100; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                } else
+                {
+                    // Put the thread to sleep for a short time
+                    // Otherwise the process will finish way too quickly.
+                    System.Threading.Thread.Sleep(25);
+
+                    worker.ReportProgress(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Defines what to do when the worker progresses in it's work.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerOne_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressBarOne.Value = e.ProgressPercentage;
+        }
+
+        /// <summary>
+        /// What the worker is to do when he stops. whether from cancelling, error or completion.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerOne_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                StatusLblOne.Text = "(Cancelled)";
+                StatusLblOne.ForeColor = Color.Red;
+            }
+            else if (e.Error != null)
+            {
+                StatusLblOne.Text = "(Error!!)";
+                StatusLblOne.ForeColor = Color.Red;
+            }
+            else
+            {
+                StatusLblOne.Text = "(Done!)";
+                StatusLblOne.ForeColor = Color.Green;
+                ProgressBarOne.Value = 100;
+            }
+        }
+        #endregion
+        
+        #region WorkerTwo
+
+        /// <summary>
+        /// Tell the worker what to do.
+        /// <br></br>
+        /// Calls the ReportProgress method.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerTwo_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            int value = (int)e.Argument;
+
+            for (int i = value; i <= 100; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    // Put the thread to sleep for a short time
+                    // Otherwise the process will finish way too quickly.
+                    System.Threading.Thread.Sleep(75);
+
+                    // Report progress triggers the progress changed event.
+                    worker.ReportProgress(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Defines what to do when the worker progresses in it's work.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerTwo_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressBarTwo.Value = e.ProgressPercentage;
+        }
+
+        /// <summary>
+        /// What the worker is to do when he stops. whether from cancelling, error or completion.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerTwo_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                StatusLblTwo.Text = "(Cancelled)";
+                StatusLblTwo.ForeColor = Color.Red;
+            }
+            else if (e.Error != null)
+            {
+                StatusLblTwo.Text = "(Error!!)";
+                StatusLblTwo.ForeColor = Color.Red;
+            }
+            else
+            {
+                StatusLblTwo.Text = "(Done!)";
+                StatusLblTwo.ForeColor = Color.Green;
+                ProgressBarTwo.Value = 100;
+            }
+        }
+        #endregion
+
+        #region WorkerThree
+
+        /// <summary>
+        /// Tell the worker what to do.
+        /// <br></br>
+        /// Calls the ReportProgress method.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerThree_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            int value = (int)e.Argument;
+
+            for (int i = value; i <= 100; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    // Put the thread to sleep for a short time
+                    // Otherwise the process will finish way too quickly.
+                    System.Threading.Thread.Sleep(100);
+
+                    // Report progress triggers the progress changed event.
+                    worker.ReportProgress(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Defines what to do when the worker progresses in it's work.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerThree_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressBarThree.Value = e.ProgressPercentage;
+        }
+
+        /// <summary>
+        /// What the worker is to do when he stops. whether from cancelling, error or completion.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BGWorkerThree_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                StatusLblThree.Text = "(Cancelled)";
+                StatusLblThree.ForeColor = Color.Red;
+            }
+            else if (e.Error != null)
+            {
+                StatusLblThree.Text = "(Error!!)";
+                StatusLblThree.ForeColor = Color.Red;
+            }
+            else
+            {
+                StatusLblThree.Text = "(Done!)";
+                StatusLblThree.ForeColor = Color.Green;
+                ProgressBarThree.Value = 100;
+            }
+        }
+
+
+
+
+        #endregion
+
+        /// <summary>
+        /// Method to Trigger the different start and cancel buttons using different keydown combinations.
+        /// </summary>
+        /// <param name="sender">The form.</param>
+        /// <param name="e">The Keys you have pressed down.</param>
+        private void JMCSignalInteruptForm_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Control && e.Shift)
+            {
+                switch (e.KeyValue)
+                {
+                    case '1':
+                        StartAsyncBtnOne.PerformClick();
+                        break;
+                    case 'A':
+                        CancelAsyncBtnOne.PerformClick();
+                        break;
+                    case '2':
+                        StartAsyncBtnTwo.PerformClick();
+                        break;
+                    case 'B':
+                        CancelAsyncBtnTwo.PerformClick();
+                        break;
+                    case '3':
+                        StartAsyncBtnThree.PerformClick();
+                        break;
+                    case 'C':
+                        CancelAsyncBtnThree.PerformClick();
+                        break;
+
+                    default: 
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
